281d252e4f17f555c44cf1824f9fe7f8ff777702
PavelShumbasov <pavelshumbasov2335@gmail.com>
2022-03-15 21:00:08 +0300

    Написано welcome message

--- src/work_with_picture.py
+++ src/work_with_picture.py
@@ -1,4 +1,5 @@
 def make_picture(link, text):
     # TODO: Сделать картинку, если что-то не удалось, вернуть None вместо picture
     pass
+    # Указано для проверки, вам само собой нужно указать картинку на вашем ПК
     return open(r"C:\Users\pavel\OneDrive\Изображения\Загруженные\пароль.jpg", "rb")
